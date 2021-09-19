1dea4f3a34fbcc03be14b93f9737fbe4da4e940a
Rodrigo Souza <88948884+rodrigots98@users.noreply.github.com>
2021-09-19 12:21:36 -0300

    Add files via upload

---
+++ ex107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(x, a):
+    return x + ((a * x)/100)
+    return x   
+
+
+def diminuir(x, d):
+    n = x - ((d * x)/100)
+    return n 
+
+
+def dobro(x):
+    d = x*2
+    return d
+
+
+def metade(x):
+    m =  x / 2
+    return m
